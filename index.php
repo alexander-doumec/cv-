69ead1ab2e1579202acd020c3d5b0098f04906cf
alexander-doumec <alexander.doumec@laplateforme.io>
2024-08-20 13:08:55 +0200

    Html

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <title>Doumec Alexander</title>
         <link rel="stylesheet" href="style.css">
     </head>
-    
+
     <body>
         <header>
             <img src="Assets/Picture/phot.jpg" alt="Photo de Doumec">
@@ -15,9 +15,57 @@
         </header>
         <main>
             <section id="Expériences Professionels">
+                <h1>Expériences Professionnels</h1>
+                <div id="Plateforme">
+                    <h2>La Plateforme</h2>
+                    <p>Assemblage et démontage d'ordinateurs</p>
+                    <p>Programmation en HTML/CSS</p>
+                    <p>Programmation avec Python (POO)</p>
+                </div>
+
+                <div id="Engie Solution">
+                    <h2>Engie Solution</h2>
+                    <p>Déploiement de la fibre optique</p>
+                </div>
+                
+                <div id="Moonroof">
+                    <h2>Moonroof</h2>
+                    <p>Serveur et Barman</p>
+                </div>
 
             </section>
-            <section id="
+
+            <section id="Formations">
+                <h1>Formations</h1>
+                <div id="Bachelor">
+                    <h2>Bachelor IT-La Plateforme</h2>
+                    <p>Pédagogie par projet sur la résolution de problèmatique concrètes en autonomie</p>
+                </div>
+
+                <div id="Studi">
+                    <h2>Studi-Developpeur Web Flutter</h2>
+                    <p>Formations à distance, utilisation de framexork pour le developpement d'applications mobiles et web</p>
+                </div>
+
+            </section>
+
+            <section id="Information">
+                <h1>Informations</h1>
+                
+
+            </section>
+
+            <section id="Atouts">
+
+            </section>
+
+            <section id="Langues">
+
+            </section>
+
+            <section id="Loisirs">
+
+            </section>
         </main>
         <footer>
 
