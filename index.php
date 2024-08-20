1eb0f60b8540bef71a50fe3a9b360b7f64927fe8
alexander-doumec <alexander.doumec@laplateforme.io>
2024-08-20 11:30:32 +0200

    mise en page +

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php include 'includes/_head.php'; ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Doumec Alexander</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
     <body>
         <header>
-            
+            <img src="Assets/Picture/phot.jpg" alt="Photo de Doumec">
+            <h1> Doumec Alexander </h1>
+            <p> Developpeur Web </p>
         </header>
         <main>
+            <section id="Expériences Professionels">
 
+            </section>
+            <section id="
         </main>
         <footer>
-            
+
         </footer>
     </body>
 </html>
