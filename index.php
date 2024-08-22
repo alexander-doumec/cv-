479b4e48845883f486090daf2c3b95b377728b6c
alexander-doumec <alexander.doumec@laplateforme.io>
2024-08-22 13:49:35 +0200

    update 2.2

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Doumec Alexander</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="Assets/Css/style.css">
-    <link rel="stylesheet" href="Assets/Css/print.css">
-</head>
+
+  <?php include("includes/_head.php"); ?>
+
 
 <body>
     <header class="bg-success text-white text-center py-4">
